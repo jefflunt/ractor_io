0c6f4d5546649ef92a22b85198e0f85059dc2c22
jefflunt <jefflunt@gmail.com>
2023-01-02 10:46:53 -0600

    support optional move paramters, which controls whether Ractor::yield will use :move (shared memory, seems unreliable) or not (copied memory, reliable, but consumes a lot more memory)

--- lib/ractor_io.rb
+++ lib/ractor_io.rb
@@ -15,7 +15,23 @@
 class RactorIO
   attr_reader :string
 
-  def initialize
+  # move: if true, the Ractor will yield the loaded file using the `move` option of Ractor::yield.
+  #   default: false
+  #
+  #   NOTE: as of this writing, and having tested this against Ruby 3.1.x and
+  #   3.2.x, setting the `move` option to `true' for large amounts of data
+  #   seems to cause a crash, at least on macOS. as a result this option is set
+  #   to `false` by default, which will result in a lot of memory copying, and
+  #   therefore increasing total memory consumption.
+  #
+  #   In the meantime, see also https://github.com/jefflunt/thread_io -
+  #   basically the same library as this, but using Threads only instead of
+  #   Ractors, which has the advantage of sharing memory, if that's important to
+  #   you. I haven't done extensive performance testing, so the perfornace
+  #   difference between ractor_io and thread_io might not be very large for
+  #   your particular use case.
+  def initialize(move: false)
+    @move = move
     @ready = false
     @string = nil
   end
@@ -28,7 +44,7 @@ class RactorIO
       @ready = false
       @string = nil
       r = Ractor.new do
-        Ractor.yield(IO.read(Ractor.receive), move: true)
+        Ractor.yield(IO.read(Ractor.receive), move: @move)
       end
       r.send(path)
       @string = r.take
